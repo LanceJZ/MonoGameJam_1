ebeae48b8c64b2fda040aeb5bf22054fbc3e1eb5
LanceJZ <lance.zimmerman@gmail.com>
2018-02-10 19:22:32 -0800

    Add project files.

---
+++ MonoGameJam_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MonoGameJam_1
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MonoGameJam_1())
+                game.Run();
+        }
+    }
+}
